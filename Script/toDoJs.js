ea6bcd361c1dd87622f28a413e3cf833988704c6
kbhari90 <kbhari90@outlook.com>
2014-10-24 17:24:25 +0530

    Added alert for saveDBData onsuccess

--- Script/toDoJs.js
+++ Script/toDoJs.js
@@ -250,7 +250,7 @@ function saveDBData(){
         };
         
         request.onsuccess = function(event){
-            console.log("Successfully added data");
+            alert("Successfully added data");
             for(var i=0;i<arrayListItems.length;i++)
             console.log(arrayListItems[i]);
         };
